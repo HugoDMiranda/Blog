0b347991a70c4c0c42a05a78d3b9a122191aef90
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-11 22:23:34 -0500

    Home component update

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,15 @@ import React from "react";
 import "../sass/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <h2>Home</h2>
+      <div>
+        <h4>Articulos</h4>
+        <h4>Ultimos articulos</h4>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
