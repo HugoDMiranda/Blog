67cf3621aa2211c5ecb396f5b1f2c0ada9b61728
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-16 20:49:26 -0500

    component blogSections update

--- src/components/BlogsSections.js
+++ src/components/BlogsSections.js
@@ -3,9 +3,26 @@ import "../sass/components-styles/BlogsSections.css";
 
 function BlogsSections() {
   return (
-    <div className="sections">
-      <h3>BlogsSections</h3>
-    </div>
+    <section className="sections">
+      <div className="sections-info">
+        <h2>Title</h2>
+        <p>Date 00/00/00 at 00:00</p>
+      </div>
+      <div className="sections-sum">
+        <p>Topics</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus
+          scelerisque ligula id hendrerit. Morbi in ultricies nunc. Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Morbi et lectus sit amet diam facilisis pretium. Nunc lacus
+          metus, consequat eget consequat et, lobortis vel felis. Cras eu dui
+          tortor. Duis vulputate imperdiet ligula. Suspendisse vel velit eget
+          justo porta porttitor nec in justo. Nam iaculis vehicula lacus, nec
+          sagittis libero convallis non. Sed faucibus risus non dapibus
+          tincidunt.
+        </p>
+      </div>
+    </section>
   );
 }
 
