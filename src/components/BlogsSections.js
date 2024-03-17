108d340d635dc4cc7b4cd58181921f96b98c5833
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-16 21:22:12 -0500

    component blogSections update

--- src/components/BlogsSections.js
+++ src/components/BlogsSections.js
@@ -2,26 +2,35 @@ import React from "react";
 import "../sass/components-styles/BlogsSections.css";
 
 function BlogsSections() {
+  const Test = [1, 2, 3, 4, 5];
+
   return (
     <section className="sections">
-      <div className="sections-info">
-        <h2>Title</h2>
-        <p>Date 00/00/00 at 00:00</p>
-      </div>
-      <div className="sections-sum">
-        <h5>Topics</h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus
-          scelerisque ligula id hendrerit. Morbi in ultricies nunc. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos. Morbi et lectus sit amet diam facilisis pretium. Nunc lacus
-          metus, consequat eget consequat et, lobortis vel felis. Cras eu dui
-          tortor. Duis vulputate imperdiet ligula. Suspendisse vel velit eget
-          justo porta porttitor nec in justo. Nam iaculis vehicula lacus, nec
-          sagittis libero convallis non. Sed faucibus risus non dapibus
-          tincidunt.
-        </p>
-      </div>
+      {Test.map((test) => {
+        return (
+          <>
+            <div className="sections-info">
+              <h2>Title</h2>
+              <p>Date 00/00/00 at 00:00</p>
+            </div>
+            <div className="sections-sum">
+              <h5>Topics</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                finibus scelerisque ligula id hendrerit. Morbi in ultricies
+                nunc. Class aptent taciti sociosqu ad litora torquent per
+                conubia nostra, per inceptos himenaeos. Morbi et lectus sit amet
+                diam facilisis pretium. Nunc lacus metus, consequat eget
+                consequat et, lobortis vel felis. Cras eu dui tortor. Duis
+                vulputate imperdiet ligula. Suspendisse vel velit eget justo
+                porta porttitor nec in justo. Nam iaculis vehicula lacus, nec
+                sagittis libero convallis non. Sed faucibus risus non dapibus
+                tincidunt.
+              </p>
+            </div>
+          </>
+        );
+      })}
     </section>
   );
 }
