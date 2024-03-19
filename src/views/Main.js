22c075404d6ef610c72c02300988c794697c03c7
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-19 14:34:16 -0500

    component Main update

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,7 +2,33 @@ import React from "react";
 import "../sass/Main.css";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <div className="main-info">
+        <div>
+          <img src="img/tes3.png" alt="user-img" />
+          <h4>Jane Doe</h4>
+        </div>
+
+        <h2>Title</h2>
+        <p>Date 00/00/00 at 00:00</p>
+      </div>
+      <div className="main-blog">
+        <h5>Topics</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus
+          scelerisque ligula id hendrerit. Morbi in ultricies nunc. Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Morbi et lectus sit amet diam facilisis pretium. Nunc lacus
+          metus, consequat eget consequat et, lobortis vel felis. Cras eu dui
+          tortor. Duis vulputate imperdiet ligula. Suspendisse vel velit eget
+          justo porta porttitor nec in justo. Nam iaculis vehicula lacus, nec
+          sagittis libero convallis non. Sed faucibus risus non dapibus
+          tincidunt.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Main;
