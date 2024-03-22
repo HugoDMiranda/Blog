f670272b849ba338116ff020b5473256f51191b3
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-21 23:04:46 -0500

    component Similar added

---
+++ src/components/Similar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Similar() {
+  return (
+    <div className="similar">
+      <div className="similar-info">
+        <img src="img/tes3.png" alt="user-img" />
+        <h2>Title</h2>
+      </div>
+      <div className="similar-blog">
+        <h5>Topics</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus
+          scelerisque ligula id hendrerit. Morbi in ultricies nunc. Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos...
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Similar;
