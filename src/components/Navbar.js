cb4ba4f152445c2dc62650e1a5d32c182f518552
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-29 17:11:00 -0500

    component Navbar elements update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { CiCloudOn } from "react-icons/ci";
+import { GoHome } from "react-icons/go";
+// import { Link } from "react-router-dom";
 import "../sass/components-styles/Navbar.css";
 
 function Navbar() {
+  const Test = [1, 2, 3];
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="nav">
-      <Link to="/">Navbar</Link>
-    </nav>
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="inner">
+        <header>
+          <button
+            type="button"
+            className="sidebar-burger"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className="icon">{isOpen ? "close" : "menu"}</span>
+          </button>
+          <GoHome />
+        </header>
+        <nav>
+          {Test.map((item) => {
+            return (
+              <button key={item} type="button">
+                <CiCloudOn className="icon">item</CiCloudOn>
+                <p>item</p>
+              </button>
+            );
+          })}
+        </nav>
+      </div>
+    </aside>
   );
 }
 
