ce70abd5fbd9b5dafdd17f9065e1c51ee066bc1e
HugoDMiranda <hdmirandab@hotmail.com>
2024-03-30 00:49:25 -0500

    component Navbar elements update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,85 @@
 import { useState } from "react";
-import { CiCloudOn } from "react-icons/ci";
-import { GoHome } from "react-icons/go";
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import "../sass/components-styles/Navbar.css";
 
 function Navbar() {
   const Test = [1, 2, 3];
   const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const menuItem = [
+    {
+      path: "/",
+      name: "Dashboard",
+      icon: <FaTh />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/analytics",
+      name: "Analytics",
+      icon: <FaRegChartBar />,
+    },
+    {
+      path: "/comment",
+      name: "Comment",
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: "/product",
+      name: "Product",
+      icon: <FaShoppingBag />,
+    },
+    {
+      path: "/productList",
+      name: "Product List",
+      icon: <FaThList />,
+    },
+  ];
 
   return (
-    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
-      <div className="inner">
-        <header>
-          <button
-            type="button"
-            className="sidebar-burger"
-            onClick={() => setIsOpen(!isOpen)}
+    <div className="container">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            Logo
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassName="active"
           >
-            <span className="icon">{isOpen ? "close" : "menu"}</span>
-          </button>
-          <GoHome />
-        </header>
-        <nav>
-          {Test.map((item) => {
-            return (
-              <button key={item} type="button">
-                <CiCloudOn className="icon">item</CiCloudOn>
-                <p>item</p>
-              </button>
-            );
-          })}
-        </nav>
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{ display: isOpen ? "block" : "none" }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
       </div>
-    </aside>
+      <main>Children</main>
+    </div>
   );
 }
 
