538db02c048049e48cb36da565f5d979d22f693c
HugoDMiranda <hdmirandab@hotmail.com>
2024-04-25 23:28:20 -0500

    view User style  added

--- src/views/User.js
+++ src/views/User.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "../sass/User.css";
 
 function User() {
   return (
-    <div>
-      <div>
-        <img src="img/tes3.png" alt="user-img" className="user-img" />
+    <div className="user">
+      <div className="user-info">
+        <img src="img/tes3.png" alt="user-img" className="user-info-img" />
         <h4>Jane Doe</h4>
       </div>
-      <div>
+      <div className="user-button">
         <button>Favorites</button>
         <button>Yours</button>
       </div>
-      <div>
+      <div className="user-posts">
         <h3>POSTS</h3>
       </div>
     </div>
