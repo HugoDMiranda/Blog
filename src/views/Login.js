ca94c5a68769c49529db58727648d8a6808a6f81
HugoDMiranda <hdmirandab@hotmail.com>
2024-05-01 13:31:15 -0500

    view Login  elements update

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../sass/Login.css";
 
 function Login() {
+  const [login, setLogin] = useState(true);
+
   return (
     <div className="login">
       <div className="login-account">
-        <h3>Login</h3>
-        <h4>Create account</h4>
+        <h4>Welcome to Blogs</h4>
+        {login ? (
+          <>
+            <h3>User name or email</h3>
+            <input />
+            <h4>Password</h4>
+            <input />
+            <spam onClick={() => setLogin(false)}>
+              Don`t have a account? Sing in
+            </spam>
+            <button>Login</button>
+          </>
+        ) : (
+          <>
+            <h3>User name</h3>
+            <input />
+            <h3>Email</h3>
+            <input />
+            <h4>Password</h4>
+            <input />
+            <h4>Confirms Password</h4>
+            <input />
+            <spam onClick={() => setLogin(true)}>
+              You already have an account? Login
+            </spam>
+            <button>Sing in</button>
+          </>
+        )}
       </div>
       <div className="login-info">
         <h2>!WELCOME!</h2>
