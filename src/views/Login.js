13c52668653db8c828311bc687f7da8cee5aba0b
HugoDMiranda <hdmirandab@hotmail.com>
2024-05-02 23:19:38 -0500

    view Login style update

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,32 +7,38 @@ function Login() {
   return (
     <div className="login">
       <div className="login-account">
-        <h4>Welcome to Blogs</h4>
+        <h4 className="login-account-title">Welcome to Blogs</h4>
         {login ? (
           <>
-            <h3>User name or email</h3>
-            <input />
-            <h4>Password</h4>
-            <input />
-            <spam onClick={() => setLogin(false)}>
+            <h3 className="login-account-text">User name or email</h3>
+            <input className="login-account-input" />
+            <h4 className="login-account-text">Password</h4>
+            <input className="login-account-input" />
+            <spam
+              className="login-account-login"
+              onClick={() => setLogin(false)}
+            >
               Don`t have a account? Sing in
             </spam>
-            <button>Login</button>
+            <button className="login-account-button">Login</button>
           </>
         ) : (
           <>
-            <h3>User name</h3>
-            <input />
-            <h3>Email</h3>
-            <input />
-            <h4>Password</h4>
-            <input />
-            <h4>Confirms Password</h4>
-            <input />
-            <spam onClick={() => setLogin(true)}>
+            <h3 className="login-account-text">User name</h3>
+            <input className="login-account-input" />
+            <h3 className="login-account-text">Email</h3>
+            <input className="login-account-input" />
+            <h4 className="login-account-text">Password</h4>
+            <input className="login-account-input" />
+            <h4 className="login-account-text">Confirms Password</h4>
+            <input className="login-account-input" />
+            <spam
+              className="login-account-login"
+              onClick={() => setLogin(true)}
+            >
               You already have an account? Login
             </spam>
-            <button>Sing in</button>
+            <button className="login-account-button">Sing in</button>
           </>
         )}
       </div>
