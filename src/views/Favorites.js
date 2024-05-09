e47ed3391afe4c6d0f7968906333947b41ac072a
HugoDMiranda <hdmirandab@hotmail.com>
2024-05-09 00:02:00 -0500

    view Favorites elements update

--- src/views/Favorites.js
+++ src/views/Favorites.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Favorites() {
+  const [changePassword, setChangePassword] = useState(false);
+
   return (
-    <div>
-      <div>
-        <img src="img/tes3.png" alt="user-img" className="user-info-img" />
+    <div className="account">
+      <div className="account-info">
+        <img
+          src="img/tes3.png"
+          alt="account-img"
+          className="account-info-img"
+        />
         <h4>Jane Doe</h4>
         <p>Last login</p>
       </div>
-      <div>
+      <div className="account-data">
         <p>Name</p>
         <p>Email</p>
         <p>number</p>
         <p>Password</p>
       </div>
-      <button>Want to change your password?</button>
-      <div>
-        <p>Your password</p>
-        <input />
-        <p>Confims password</p>
-        <input />
-        <p>New password</p>
-        <input />
-        <button>Change password</button>
-      </div>
+      <button onClick={() => setChangePassword(!changePassword)}>
+        {changePassword
+          ? `It's Okey my old password`
+          : `Want to change your password?`}
+      </button>
+      {changePassword ? (
+        <div className="account-newpassword">
+          <p>Your password</p>
+          <input />
+          <p>Confims password</p>
+          <input />
+          <p>New password</p>
+          <input />
+          <button>Change password</button>
+        </div>
+      ) : null}
     </div>
   );
 }
