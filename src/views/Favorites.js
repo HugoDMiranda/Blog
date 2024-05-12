1d5ca9005bdd2089f26648c70ba1fab4a112aa2e
HugoDMiranda <hdmirandab@hotmail.com>
2024-05-12 16:37:11 -0500

    view Favorites Formik custom error message added

--- src/views/Favorites.js
+++ src/views/Favorites.js
@@ -12,13 +12,13 @@ function Favorites() {
 
   const passwordSchema = yup.object().shape({
     password: yup.string().required(),
-    confirmspassword: yup.string().required(),
+    confirmpassword: yup.string().required(),
     newpassword: yup.string().required(),
   });
 
   const initialValues = {
     password: "",
-    confirmspassword: "",
+    confirmpassword: "",
     newpassword: "",
   };
 
@@ -62,20 +62,27 @@ function Favorites() {
             <ErrorMessage
               name="password"
               component="span"
-              className="errorMessage"
+              render={() => (
+                <span className="errorMessage">Password is required</span>
+              )}
             />
+
             <h3>Confims password</h3>
             <Field
               className="account-newpassword-input"
               autoComplete="off"
               type="password"
-              id="confirmspassword"
-              name="confirmspassword"
+              id="confirmpassword"
+              name="confirmpassword"
             />
             <ErrorMessage
-              name="confirmspassword"
+              name="confirmpassword"
               component="span"
-              className="errorMessage"
+              render={() => (
+                <span className="errorMessage">
+                  Confirm password is required
+                </span>
+              )}
             />
             <h3>New password</h3>
             <Field
@@ -88,7 +95,9 @@ function Favorites() {
             <ErrorMessage
               name="newpassword"
               component="span"
-              className="errorMessage"
+              render={() => (
+                <span className="errorMessage">New password is required</span>
+              )}
             />
             <button type="submit">Change password</button>
           </Form>
