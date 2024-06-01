e758a67b059ecd04d5eb56c230759ff409aab762
HugoDMiranda <hdmirandab@hotmail.com>
2024-05-31 23:26:57 -0700

    component Topics Link added

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../sass/components-styles/Topics.css";
 import { MdTheaterComedy } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function Topics({ topic }) {
   return (
-    <div className="topic animate__animated animate__fadeInRight">
+    <Link to="/Main" className="topic animate__animated animate__fadeInRight">
       <MdTheaterComedy className="topic-logo" size={"90px"} />
       <h3 className="topic-text">{topic}</h3>
-    </div>
+    </Link>
   );
 }
 
